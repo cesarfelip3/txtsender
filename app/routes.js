975f01fb1b6eaa7a6b4f7fad096c70d8af411112
Cesar Felipe <cesarfelip3@gmail.com>
2016-08-25 23:03:48 -0600

    set the routes in a separated file

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
