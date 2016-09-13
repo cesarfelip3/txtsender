33a126575c9e8ca5871d6ab9954009cebd336b0c
jmkane <jkane76@outlook.com>
2016-09-12 18:23:29 -0700

    Additional error handling
    Added data entry instructions for phone numbers.
    Added error on the phone transmission

--- app/routes.js
+++ app/routes.js
@@ -30,12 +30,12 @@ router.post('/send_free_sms', function (req, res) {
   console.log(phonenumber);
 
   var c = new TMClient('felipealves', 'iv8agCnjIO5qPfqrLxExRIkDkkdMWh');
-  c.Messages.send({text: msg, phones: phonenumber}, function(err, res){
-    console.log('Messages.send()', err, res);
-  });
-
-  res.send('Your menssage was sent.');
-
-
+  var err;
+  var res;
+  c.Messages.send({text:msg, phones:phonenumber}, function(err, res){
+    console.log('Messages.send()', err, res)};
+    if(err === null) then c.Messages.send({'successful transmission',phones:sender},function(err,res){console.log('Messages.send()',err,res)};
+    else c.Messages.send({text:err,phones:sender},function(err,res)){console.log('Messages.send()',err,res)};
+  );
 
 });
